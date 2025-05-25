304366aca4d91f4277fae25af4116a9e1b3bc138
krozeo <q2077681739@163.com>
2025-05-25 16:06:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask,render_template
-app=Flask(__name__) 
+from flask import Flask
+app = Flask(__name__)
+
 @app.route('/')
-def index():
-    # my_variable="Hello,krozeo!"
-    # my_list=[1,2,3,4,5]
-    # return render_template('index.html',my_list=my_list)
-    return render_template('child.html')
+def hello_world():
+    return 'Hello world!'
+@app.route('/user/<name>')
+def user(name):
+    return 'Hello, {}'.format(name)
